172af2fbde5290765ce91873e5095641571fc7bd
amedinaaa <abhram_medina@yahoo.com>
2024-04-28 22:26:33 -0700

    new commit

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/convert', methods=['GET'])
-def convert_celsius_to_fahrenheit():
-    # Check if "celsius" is provided in the query string
+def convert_f_to_c():
+    # Check if "fahrenheit" is in the query string
     if 'fahrenheit' in request.args:
         fahrenheit = float(request.args['fahrenheit'])
         celsius = (fahrenheit - 32) * 5/9
@@ -13,7 +13,7 @@ def convert_celsius_to_fahrenheit():
             'Celsius': celsius
         }), 200
     else:
-        return "Error: No celsius field provided. Please specify an amount.", 400
+        return "Error: No fahrenheit field provided. Please specify an amount.", 400
 
 if __name__ == '__main__':
     app.run(debug=True)
