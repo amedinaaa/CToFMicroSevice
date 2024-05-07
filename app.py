ec79d8406f834dd81de8c27fed9fca8102bdb09e
amedinaaa <abhram_medina@yahoo.com>
2024-05-07 12:39:08 -0700

    Updating

--- app.py
+++ app.py
@@ -5,18 +5,17 @@ app = Flask(__name__)
 @app.route('/convert', methods=['GET'])
 def convert_f_to_c():
     # Check if "fahrenheit" is in the query string
-    if 'fahrenheit' in request.args:
-        fahrenheit = float(request.args['fahrenheit'])
-        if fahrenheit is None:
-            return "Error: Please specify an amount.", 400
+    fahrenheit = request.args.get('fahrenheit')
+    if fahrenheit is None:
+        return "Error: Please specify an amount.", 400
         
-        try: 
-            fahrenheit_value = float(fahrenheit)
-        except ValueError:
-            return "Error: invalid value for fahrenheit. Please provide a valid number"
+    try: 
+        fahrenheit_value = float(fahrenheit)
+    except ValueError:
+        return "Error: invalid value for fahrenheit. Please provide a valid number", 400
         
-        celsius = (fahrenheit_value - 32) * 5/9
-        return jsonify({
+    celsius = (fahrenheit_value - 32) * 5/9
+    return jsonify({
             #'Fahrenheit given': fahrenheit,
             'Celsius': celsius
         }), 200
